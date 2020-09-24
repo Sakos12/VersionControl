988bde886d01f230c96868ab41fe6e5cd43fa9de
Sakos12 <solymossy.akos@gmail.com>
2020-09-24 12:42:42 +0200

    LastName helyett FullName mindenhol, FirstName-k törlése

--- Version/Version/Form1.cs
+++ Version/Version/Form1.cs
@@ -17,16 +17,16 @@ namespace Version
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.LastName;
-            label2.Text = Resource1.FirstName;
+            label1.Text = Resource1.FullName;
+
             button1.Text = Resource1.Add;
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
             listBox1.DisplayMember = "FullName";
             var u = new User()
             {
-                LastName = textBox1.Text,
-                FirstName = textBox2.Text
+                FullName = textBox1.Text,
+                
             };
             users.Add(u);
 
