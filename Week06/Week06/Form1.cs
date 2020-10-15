4237cae45d7795433520a7b334d27a6dbe3d2dd0
Sakos12 <solymossy.akos@gmail.com>
2020-10-15 17:10:01 +0200

    Combbox ás datetimepickerek tulajdonságainak kihasználása

--- Week06/Week06/Form1.cs
+++ Week06/Week06/Form1.cs
@@ -21,6 +21,11 @@ namespace Week06
         {
             InitializeComponent();
             GetExchangeRates();
+            RefreshData();
+        }
+        private void RefreshData()
+        {
+            Rates.Clear();
             dataGridView1.DataSource = Rates;
         }
         private void GetExchangeRates()
@@ -29,9 +34,9 @@ namespace Week06
 
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString(),
             };
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
@@ -66,6 +71,21 @@ namespace Week06
                 chartArea.AxisY.MajorGrid.Enabled = false;
                 chartArea.AxisY.IsStartedFromZero = false;
             }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
