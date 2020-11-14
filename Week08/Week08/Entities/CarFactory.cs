fb06e6e8ab0d277c3471b2c3a51b9e66885f4de2
Sakos12 <solymossy.akos@gmail.com>
2020-11-14 17:08:09 +0100

    9. feladat

--- Week08/Week08/Entities/CarFactory.cs
+++ Week08/Week08/Entities/CarFactory.cs
@@ -7,11 +7,11 @@ using Week08.Abstractions;
 
 namespace Week08.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory: IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
